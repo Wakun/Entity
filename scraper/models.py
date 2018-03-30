789c69bd5b6625ac583c10a25e28c6db5894a43e
Wakun <msadlek2@gmail.com>
2018-03-31 01:13:35 +0200

    initial commit

--- scraper/models.py
+++ scraper/models.py
@@ -3,20 +3,20 @@ from django.db import models
 
 class Product(models.Model):
 
-    art_name = models.CharField('Nazwa artykułu', max_length=255)
+    art_name = models.CharField('Nazwa artykułu', max_length=255, null=True, blank=True)
     plu_num = models.IntegerField('Numer krótki', default=0)
-    category = models.ForeignKey('Category', on_delete=models.CASCADE)
+    category = models.ForeignKey('Category', on_delete=models.CASCADE, null=True, blank=True)
 
     auchan_price = models.FloatField('Cena Auchan', null=True, blank=True)
 
     rtveuro_price = models.FloatField('Cena RTVEuroAGD', null=True, blank=True)
-    rtveuro_url = models.CharField('Link do RTVEuroAGD', null=True, blank=True)
+    rtveuro_url = models.CharField('Link do RTVEuroAGD', null=True, blank=True, max_length=1000)
 
     mediamarkt_price = models.FloatField('Cena Media Markt', null=True, blank=True)
-    mediamarkt_url = models.CharField('Link do Media Markt', null=True, blank=True)
+    mediamarkt_url = models.CharField('Link do Media Markt', null=True, blank=True, max_length=1000)
 
     mediaexpert_price = models.FloatField('Cena Media Expert', null=True, blank=True)
-    mediaexpert_url = models.CharField('Link do Media Expert', null=True, blank=True)
+    mediaexpert_url = models.CharField('Link do Media Expert', null=True, blank=True, max_length=1000)
 
     class Meta:
         verbose_name = 'Produkt'
@@ -26,7 +26,7 @@ class Product(models.Model):
 
 class Category(models.Model):
 
-    category_name = models.CharField('Kategoria', max_length=255)
+    category_name = models.CharField('Kategoria', max_length=255, null=True, blank=True)
 
     class Meta:
         verbose_name = 'Kategoria'
